ab71d41869649d10f52cab3efbe8e0ce932a1118
rfahrozi <rfahrozi@example.com>
2026-06-20 23:26:21 +0700

    fix: add production block to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -29,5 +29,21 @@ module.exports = {
     },
     migrations: { directory: path.join(__dirname, 'migrations') },
     seeds: { directory: path.join(__dirname, 'seeds') }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.DB_PORT || 5432,
+      user: process.env.DB_USER || 'satya_admin',
+      password: process.env.DB_PASSWORD || 'secure_password_here',
+      database: process.env.DB_NAME || 'satya_db',
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: { directory: path.join(__dirname, 'migrations') },
+    seeds: { directory: path.join(__dirname, 'seeds') }
   }
 };
